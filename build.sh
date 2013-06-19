0c8966deb7f53f926b93e5496eacce26e650e3a2
Aaron Jacobs <jacobsad@mcmaster.ca>
2013-06-19 04:03:09 -0700

    Added build and setup scripts.
    Signed-off-by: Aaron Jacobs <jacobsad@mcmaster.ca>

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+tex_path='/usr/texbin'
+
+trash_files='Thesis.run.xml Thesis.synctex.gz ImportantRefs.bib.bak'
+
+if [ "$1" == '--clean' ]; then
+	echo 'Cleanup requested.';
+	$tex_path/latexmk --c;
+	for file in $trash_files; do
+		if [ -a $file ]; then
+			trash -v "$file"
+		fi
+	done
+	exit 0;
+fi
+
+# First, run knitr
+Rscript -e "library(knitr); knit('Thesis.Rnw')";
+
+# Since that isn't powerful enough to deal 
+# with all the cross-referencing, run it 
+# through pdflatex again.
+$tex_path/latexmk -cd -pdflatex="$tex_path/pdflatex -interaction=nonstopmode -synctex=1" -f -pdf Thesis.tex
---
+++ setup_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# First, cd into the project directory.
+
+# Sets up a working environment similar to 
+# Rstudio... but without the soul-crushing 
+# memory leaks.
+
+r_version=$(R --version | sed -n 's/^R version \(.*\) (\(.*\)/\1/p')
+
+# New tmux window setup
+tmux new-window -n thesis -d "figlet -f slant R WORKSPACE && echo 'R v. $r_version -- Console Session #1' && R --no-save --quiet";
+tmux split-window -v -t main:thesis "echo 'R v. $r_version -- Console Session #2' && R --no-save --quiet";
+tmux split-window -v -t main:thesis;
+tmux select-layout -t thesis f5cb,124x75,0,0[124x22,0,0,19,124x22,0,23,20,124x29,0,46,21];
+tmux select-pane -t 2;
+
+subl --project ./thesis.sublime-project
